8c51426c8fea0a95f327b746bb217992854b44cc
Sigifredo Chacón Balladares <24.sigi@estudiantec.cr>
2024-10-04 10:38:30 -0600

    se edito el db config

--- DBConfig.js
+++ DBConfig.js
@@ -1,8 +1,8 @@
 export const DBConfig = {
-    host: 'db-pi-grupo02.cxsgumuqu088.us-east-2.rds.amazonaws.com', // Cambia localhost por el host de la base de datos de AWS
-    user: 'admin', // Usuario de la base de datos en AWS
-    port: 3306, // El puerto MySQL por defecto
-    password: 'Pigrupo022024', // Contraseña de tu base de datos en AWS
-    database: 'sistemacaa', // Nombre de la base de datos que estás usando en RDS
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: 'root',
+    database: 'sistemacaa',
 };
 
