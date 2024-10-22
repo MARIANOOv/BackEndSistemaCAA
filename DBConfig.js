0cd1c8b3944594b21142d023e597ff70ce6ac15b
Mariano Oconitrillo Vega <m.oco.vega@estudiantec.cr>
2024-10-21 23:08:30 -0600

    Prueba

--- DBConfig.js
+++ DBConfig.js
@@ -1,8 +1,8 @@
 export const DBConfig = {
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: '1811',
-    database: 'sistemacaa',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    port: process.env.DB_PORT ||3306,
+    password: process.env.DB_PASSWORD || '12345678',
+    database: process.env.DB_NAME ||'sistemacaa',
 };
 
